735a1b531039e3f64d2a5e85715e746920dd3acd
RomaCarbonellITB <145445264+RomaCarbonellITB@users.noreply.github.com>
2025-02-18 16:55:55 +0100

    Update build_run.sh

--- build_run.sh
+++ build_run.sh
@@ -5,10 +5,6 @@ CONTAINER_NAME="a1_p2"
 echo "Creant imatge..."
 docker build -t $IMAGE_NAME .
 
-echo "Imatge" $IMAGE_NAME "creada."
-echo "Creant contenidor..."
-docker run -dit --name $CONTAINER_NAME -p 5901:5901 -p 2222:22 $IMAGE_NAME bash
-
 if [ $? -eq 0 ]; then
     echo "El contenidor s'ha creat i s'està executant correctament!"
 else
